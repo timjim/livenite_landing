f344d50da6e368fd4966dd38ff1a4135ed3bd149
Tim Elder <timothy.j.elder@gmail.com>
2014-07-28 19:21:57 +0100

    Section 1 non-responsive complete

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -7,28 +7,52 @@ $(function() {
 
   var s = skrollr.init({forceHeight: false});
 
-  $('#section_2').waypoint(function() {
-    console.log('hello');
+  $('#section_2').waypoint(function(direction) {
+    if(direction==='down') {
+      $('#fixed_header').css('opacity', 0).slideDown('slow').animate(
+        { opacity: 1 },
+        { queue: false, duration: 'slow' }
+      );
+      console.log('down');
+    }else{
+      $('#fixed_header').css('opacity', 1).slideUp('slow').animate(
+        { opacity: 0 },
+        { queue: false, duration: 'slow' }
+      );
+      console.log('up');
+    }
   }, {
     offset: '20%'
   });
 
   $('.down:eq(0)').click(function() {
     $('html, body').animate({
-      scrollTop: $("#section_2").offset().top
+      scrollTop: $("#section_2").offset().top -100
     }, 1000);
   })
 
+  $('#slide').show();
+
+  $('#cycle').cycle({ 
+    fx:'scrollLeft',
+    timeout:  4000 
+  });
+
   fadeInLoad('#logo', 0, 'fadeInDown');
-  fadeInLoad('#about', 600, 'fadeIn');
-  fadeInLoad('#usp_1', 600, 'fadeIn');
-  fadeInLoad('#usp_2', 600, 'fadeIn');
-  fadeInLoad('#downloads', 800, 'fadeIn');
-  fadeInLoad('.down:eq(0)', 800, 'fadeIn');
+  fadeInLoad('#about', 400, 'fadeIn');
+  fadeInLoad('.usp', 800, 'fadeIn');
+  fadeInLoad('#downloads', 1000, 'fadeIn');
+  fadeInLoad('#bottom', 1000, 'fadeIn');
 
   function fadeInLoad(el, delayTime, anim) {
     $(el).addClass('hidden').delay(delayTime).queue(function(next){
       $(this).removeClass('hidden').addClass(anim +' animated').dequeue();
+    });
+  };
+
+  function animate($el, type) {
+    $el.removeClass(type +' animated').addClass(type +' animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+      $(this).removeClass(type +' animated');
     });
   };
   
